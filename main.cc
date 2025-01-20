e2d21a94ce1d19989daf2dc4fbe3be73e5ba830c
Marko Ivanovic <ivanovic.marko@yandex.com>
2025-01-20 22:12:47 +0100

    Update for latest Chromium in Jan 2025

--- main.cc
+++ main.cc
@@ -39,7 +39,7 @@
 class URLRequestDelegate : public net::URLRequest::Delegate {
  public:
   explicit URLRequestDelegate(base::OnceClosure finish_callback)
-      : buf_(base::MakeRefCounted<net::IOBuffer>(kBufSize)),
+      : buf_(base::MakeRefCounted<net::IOBufferWithSize>(kBufSize)),
         data_helper_(std::vector<char>(kBufSize + 1)),
         finish_callback_(std::move(finish_callback)) {}
 
@@ -114,7 +114,7 @@ class URLRequestDelegate : public net::URLRequest::Delegate {
         return;
       }
 
-      std::copy(buf_->data(), buf_->data() + read_result,
+      std::copy(buf_->span().begin(), buf_->span().end(),
                 std::begin(data_helper_));
       data_helper_[read_result] = '\0';
       std::cout << data_helper_.data();
@@ -140,13 +140,14 @@ class URLRequestDelegate : public net::URLRequest::Delegate {
 
 class Request {
  public:
-  Request() {}
+  Request() = default;
 
   Request(const Request&) = delete;
   Request& operator=(const Request&) = delete;
   ~Request() = default;
 
   void StartOnIOThread(const GURL& gurl, base::OnceClosure finish_callback) {
+    LOG(ERROR) << __PRETTY_FUNCTION__ << " starting on io thread";
     DCHECK(!context_.get());
     net::URLRequestContextBuilder context_builder;
     context_builder.DisableHttpCache();
@@ -173,7 +174,7 @@ class Request {
         gurl, net::RequestPriority::HIGHEST, url_request_delegate_.get(),
         net::DefineNetworkTrafficAnnotation("chget_download", R"(
                 semantics {
-                  sender: "Chget downloader"
+                  sender: "chromium_wget"
                   description:
                     "Simple wget-like utility for downloading files using Chrome
                     stack."
@@ -210,12 +211,14 @@ class IOThreadDelegate : public base::Thread::Delegate {
       : sequence_manager_(CreateUnboundSequenceManager(
             base::sequence_manager::SequenceManager::Settings::Builder()
                 .SetMessagePumpType(base::MessagePumpType::IO)
+                .SetLogPostTask(true)
+                .SetTaskLogging(base::sequence_manager::SequenceManager::
+                                    Settings::TaskLogging::kEnabled)
                 .Build())),
         default_task_queue_(sequence_manager_->CreateTaskQueue(
             base::sequence_manager::TaskQueue::Spec(
-                base::sequence_manager::QueueName::DEFAULT_TQ))),
-        default_task_runner_(default_task_queue_->task_runner()),
-        simple_task_executor_(default_task_runner_) {}
+                base::sequence_manager::QueueName::IO_DEFAULT_TQ))),
+        default_task_runner_(default_task_queue_->task_runner()) {}
 
   IOThreadDelegate(const IOThreadDelegate&) = delete;
   IOThreadDelegate& operator=(const IOThreadDelegate&) = delete;
@@ -226,22 +229,50 @@ class IOThreadDelegate : public base::Thread::Delegate {
     return default_task_runner_;
   }
 
-  void BindToCurrentThread(base::TimerSlack timer_slack) override {
+  void BindToCurrentThread() override {
     DCHECK(sequence_manager_);
+    sequence_manager_->SetDefaultTaskRunner(default_task_runner_);
     sequence_manager_->BindToMessagePump(
         base::MessagePump::Create(base::MessagePumpType::IO));
-    sequence_manager_->SetTimerSlack(timer_slack);
-    sequence_manager_->SetDefaultTaskRunner(default_task_runner_);
-    base::SetTaskExecutorForCurrentThread(&simple_task_executor_);
   }
 
  private:
   const std::unique_ptr<base::sequence_manager::SequenceManager>
       sequence_manager_;
-  scoped_refptr<base::sequence_manager::TaskQueue> default_task_queue_;
+  base::sequence_manager::TaskQueue::Handle default_task_queue_;
   scoped_refptr<base::SingleThreadTaskRunner> default_task_runner_;
-  base::SimpleTaskExecutor simple_task_executor_;
 };
+
+class MainThreadTaskExecutor {
+ public:
+  MainThreadTaskExecutor();
+
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner() {
+    return task_runner_;
+  }
+
+ private:
+  std::unique_ptr<base::sequence_manager::SequenceManager> sequence_manager_;
+  base::sequence_manager::TaskQueue::Handle task_queue_;
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+};
+
+MainThreadTaskExecutor::MainThreadTaskExecutor()
+    : sequence_manager_(
+          base::sequence_manager::CreateSequenceManagerOnCurrentThreadWithPump(
+              base::MessagePump::Create(base::MessagePumpType::DEFAULT),
+              base::sequence_manager::SequenceManager::Settings::Builder()
+                  .SetLogPostTask(true)
+                  .SetTaskLogging(base::sequence_manager::SequenceManager::
+                                      Settings::TaskLogging::kEnabled)
+                  .SetMessagePumpType(base::MessagePumpType::DEFAULT)
+                  .Build())),
+      task_queue_(sequence_manager_->CreateTaskQueue(
+          base::sequence_manager::TaskQueue::Spec(
+              base::sequence_manager::QueueName::DEFAULT_TQ))),
+      task_runner_(task_queue_->task_runner()) {
+  sequence_manager_->SetDefaultTaskRunner(task_runner_);
+}
 
 std::unique_ptr<base::Thread> StartIOThread(
     std::unique_ptr<base::Thread::Delegate> delegate) {
@@ -289,26 +320,42 @@ URLLoader::URLLoader(
 
 void URLLoader::DownloadFromURL(const std::string& url,
                                 DownloadFromURLCallback callback) {
-  // We must create URL request on task runner bound to io thread.
+  LOG(ERROR) << "Got download request in main child task.";
+  // // We must create URL request on task runner bound to io thread.
   io_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(
           &Request::StartOnIOThread, base::Unretained(&request_), GURL(url),
           base::BindLambdaForTesting([callback = std::move(callback), url,
                                       this]() mutable {
+            LOG(ERROR) << "Finish lambda IO child task";
             main_task_runner_->PostTask(
                 FROM_HERE, base::BindLambdaForTesting(
                                [callback = std::move(callback), url]() mutable {
+                                 LOG(ERROR) << "Finish lambda main child task";
                                  std::move(callback).Run("Downloaded " + url);
                                }));
           })));
 }
 
-uint64_t GetPipeName() {
-  return 138904UL;
+void BindAndCallRemote(mojo::ScopedMessagePipeHandle pipe,
+                       std::string url_str,
+                       base::OnceClosure quit_loop) {
+  mojo::PendingRemote<url_loader::mojom::URLLoader> pending_remote(
+      std::move(pipe), 0);
+  mojo::Remote<url_loader::mojom::URLLoader> remote;
+  remote.Bind(std::move(pending_remote));
+
+  remote->DownloadFromURL(
+      url_str, base::BindLambdaForTesting([quit_loop = std::move(quit_loop)](
+                                              const std::string& msg) mutable {
+        DLOG(ERROR) << "Final message: " << msg << std::endl;
+        std::move(quit_loop).Run();
+      }));
 }
 
 int ParentMain(base::CommandLine* cmdline) {
+  LOG(ERROR) << __PRETTY_FUNCTION__ << " Starting parent";
   if (cmdline->GetArgs().size() < 1) {
     LOG(ERROR) << "Argument missing.";
     return 1;
@@ -322,13 +369,14 @@ int ParentMain(base::CommandLine* cmdline) {
     return 2;
   }
 
-  mojo::core::Init();
+  mojo::core::Init(mojo::core::Configuration{true});
+  MainThreadTaskExecutor task_executor;
   std::unique_ptr<base::Thread> io_thread =
-      StartIOThread(std::unique_ptr<base::Thread::Delegate>(nullptr));
+      StartIOThread(std::make_unique<IOThreadDelegate>());
   auto ipc_support = std::make_unique<mojo::core::ScopedIPCSupport>(
       io_thread->task_runner(),
       mojo::core::ScopedIPCSupport::ShutdownPolicy::CLEAN);
-  base::SingleThreadTaskExecutor task_executor;
+
   base::RunLoop main_run_loop;
   base::OnceClosure quit_loop = main_run_loop.QuitClosure();
 
@@ -337,30 +385,26 @@ int ParentMain(base::CommandLine* cmdline) {
       std::make_unique<base::CommandLine>(child_path);
   child_cmd_line->AppendSwitch("child_process");
 
-  uint64_t pipe_name = GetPipeName();
   mojo::OutgoingInvitation invitation;
-  mojo::ScopedMessagePipeHandle pipe = invitation.AttachMessagePipe(pipe_name);
-  mojo::PendingRemote<url_loader::mojom::URLLoader> pending_remote(
-      std::move(pipe), 0);
-  mojo::Remote<url_loader::mojom::URLLoader> remote;
-  remote.Bind(std::move(pending_remote));
+  mojo::ScopedMessagePipeHandle pipe = invitation.AttachMessagePipe("mypipe");
+
   mojo::PlatformChannel mojo_channel;
   base::LaunchOptions options;
   mojo_channel.PrepareToPassRemoteEndpoint(&options, child_cmd_line.get());
 
+  LOG(ERROR) << "Launching " << child_cmd_line->GetCommandLineString();
   base::Process process = base::LaunchProcess(*child_cmd_line, options);
 
   mojo_channel.RemoteProcessLaunchAttempted();
   mojo::OutgoingInvitation::Send(std::move(invitation), process.Handle(),
                                  mojo_channel.TakeLocalEndpoint());
 
-  remote->DownloadFromURL(
-      url_str, base::BindLambdaForTesting([quit_loop = std::move(quit_loop)](
-                                              const std::string& msg) mutable
-                                              {
-        DLOG(ERROR) << msg << std::endl;
-        std::move(quit_loop).Run();
-      }));
+  task_executor.task_runner()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&BindAndCallRemote, std::move(pipe), url_str,
+                     std::move(quit_loop)),
+      base::Seconds(1));
+  // BindAndCallRemote(std::move(pipe), url_str, std::move(quit_loop));
 
   // Loop in place till download completes.
   main_run_loop.Run();
@@ -368,15 +412,15 @@ int ParentMain(base::CommandLine* cmdline) {
 }
 
 int ChildMain(base::CommandLine* cmdline) {
+  LOG(ERROR) << __PRETTY_FUNCTION__ << " Starting child";
   mojo::core::Init();
+  MainThreadTaskExecutor task_executor;
   std::unique_ptr<base::Thread> io_thread =
       StartIOThread(std::make_unique<IOThreadDelegate>());
   auto ipc_support = std::make_unique<mojo::core::ScopedIPCSupport>(
       io_thread->task_runner(),
       mojo::core::ScopedIPCSupport::ShutdownPolicy::CLEAN);
-  base::SingleThreadTaskExecutor task_executor;
   base::RunLoop child_run_loop;
-  base::OnceClosure quit_loop = child_run_loop.QuitClosure();
 
   mojo::PlatformChannelEndpoint mojo_channel_endpoint =
       mojo::PlatformChannel::RecoverPassedEndpointFromCommandLine(*cmdline);
@@ -389,8 +433,10 @@ int ChildMain(base::CommandLine* cmdline) {
   mojo::IncomingInvitation invitation =
       mojo::IncomingInvitation::Accept(std::move(mojo_channel_endpoint));
 
+  CHECK(invitation.is_valid()) << "Invitation is invalid.";
+
   mojo::ScopedMessagePipeHandle pipe_handle =
-      invitation.ExtractMessagePipe(GetPipeName());
+      invitation.ExtractMessagePipe("mypipe");
 
   if (!pipe_handle.is_valid()) {
     LOG(ERROR) << "Invalid message pipe in mojo invitation.";
@@ -399,6 +445,8 @@ int ChildMain(base::CommandLine* cmdline) {
 
   auto pending_receiver = mojo::PendingReceiver<url_loader::mojom::URLLoader>(
       std::move(pipe_handle));
+
+  CHECK(pending_receiver.is_valid()) << "Invalid pending receiver.";
 
   URLLoader url_loader(std::move(pending_receiver), io_thread->task_runner());
 
@@ -410,6 +458,7 @@ int main(int argc, char* argv[]) {
   logging::SetLogItems(true /* enable_process_id */,
                        true /* enable_thread_id */, true /* enable_timestamp */,
                        true /* enable_tickcount */);
+  logging::SetMinLogLevel(-3);
   logging::InitLogging({});
   base::AtExitManager at_exit_manager;
   base::CommandLine::Init(argc, argv);
